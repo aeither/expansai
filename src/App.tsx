2e9a07c27ef564b6df40d24f4a2da17735fa0bc8
aeither <36173828+aeither@users.noreply.github.com>
2023-09-23 22:35:36 +0300

    expand

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,36 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import WebApp from "@twa-dev/sdk";
-import { MainButton } from "@twa-dev/sdk/react";
-import { InitialsAvatar } from "@twa-dev/mark42";
+import { MainButton, BackButton } from "@twa-dev/sdk/react";
+import {
+  InitialsAvatar,
+  AppearanceContext,
+  AppearanceProvider,
+  Cell,
+  ImageAvatar,
+  Page,
+  Section,
+  Switch,
+  useColorScheme,
+  usePlatform,
+  useTheme,
+} from "@twa-dev/mark42";
 
 WebApp.ready();
 
 function App() {
   const [count, setCount] = useState(0);
+
+  const action1 = async () => {
+    WebApp.HapticFeedback.impactOccurred("light");
+  };
+  const action2 = async () => {
+    WebApp.CloudStorage.setItem("test", Math.random().toFixed(2));
+  };
+
+  const action3 = async () => {
+    WebApp.CloudStorage.getItem("test", (_, result) => console.log(result));
+  };
 
   return (
     <>
@@ -18,6 +41,23 @@ function App() {
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <MainButton text="Submit" onClick={() => alert("submitted")} />
+        <BackButton onClick={() => alert("back")} />
+        <Page mode="secondary">
+          <Section
+            description="Share your contacts with the community. It will help other members to reach you faster."
+            header="Public contacts"
+          >
+            <Cell after={<Switch />} description="@Stambultsian">
+              Twitter
+            </Cell>
+            <Cell after={<Switch />} description="@artursupertramp">
+              Facebook
+            </Cell>
+            <Cell after={<Switch checked />} description="@ArthurStam">
+              Telegram
+            </Cell>
+          </Section>
+        </Page>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
@@ -30,6 +70,15 @@ function App() {
         <button type="button" onClick={() => WebApp.showAlert("Hey there!")}>
           click
         </button>
+        <button type="button" onClick={action1}>
+          click1
+        </button>
+        <button type="button" onClick={action2}>
+          click2
+        </button>
+        <button type="button" onClick={action3}>
+          click3
+        </button>
 
         <InitialsAvatar
           entityId={12345}
@@ -37,6 +86,11 @@ function App() {
           theme="apple"
           className="MyAvatar"
           style={{ marginRight: 10 }}
+        />
+
+        <ImageAvatar
+          size={100}
+          src="https://cdn4.telegram-cdn.org/file/GSG98YPeYhPH42R0BlvQT7di6ha48LJI4lZwbQ7q7f_TYAC5FwRrEBTlyaVij-ylfflg4aU0R8AtL8UZmG9R6sX0S12R_zLTuEUlaV99qXZm_OcaFqdknYCbxb7-XuVCy7c7mPaWagdFuF_T2-fopi1dzsrvW6E6ff8qFhQqIrrvtBdq7jO5829aCEpSIHi9QqFuw6l1WuOWfoq1LjjV7kq2wSfo_uOlN4PP-hFPAL1wklwnTw4NPE-Mfu9gFd472JUN9e299UP2f2jeZnvst2qX0Go6fb81gewClahABP-veeqGrVBxVphfixV3KJulKdt0CCU7KhFM7e-Gd4-qug.jpg"
         />
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
