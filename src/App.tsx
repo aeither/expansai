5dd3da1112c277fb4a2147ca5d31bdfd88c8a435
aeither <36173828+aeither@users.noreply.github.com>
2023-09-24 10:06:10 +0300

    switch on pnpm

--- src/App.tsx
+++ src/App.tsx
@@ -8,42 +8,30 @@ import {
 } from "@twa-dev/mark42";
 import WebApp from "@twa-dev/sdk";
 import { MainButton } from "@twa-dev/sdk/react";
-import { createWeb3Modal, defaultWagmiConfig } from "@web3modal/wagmi/react";
 import { useState } from "react";
 import "./App.css";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import { WagmiConfig } from "wagmi";
-import { arbitrum, mainnet } from "wagmi/chains";
-import { useWeb3Modal } from "@web3modal/wagmi/react";
-import SID from "@siddomains/sidjs";
-import * as SIDfunctions from "@siddomains/sidjs";
-import { providers } from "ethers";
 
-let sid;
+// // 1. Get projectId
+// if (!import.meta.env.VITE_PROJECT_ID)
+//   throw new Error("VITE_PROJECT_ID not found");
+// const projectId = import.meta.env.VITE_PROJECT_ID;
 
-// 1. Get projectId
-if (!import.meta.env.VITE_PROJECT_ID)
-  throw new Error("VITE_PROJECT_ID not found");
-const projectId = import.meta.env.VITE_PROJECT_ID;
+// // 2. Create wagmiConfig
+// const chains = [mainnet, arbitrum];
+// const wagmiConfig = defaultWagmiConfig({
+//   chains,
+//   projectId,
+//   appName: "Expansai",
+// });
 
-// 2. Create wagmiConfig
-const chains = [mainnet, arbitrum];
-const wagmiConfig = defaultWagmiConfig({
-  chains,
-  projectId,
-  appName: "Expansai",
-});
-
-// 3. Create modal
-createWeb3Modal({ wagmiConfig, projectId, chains });
+// // 3. Create modal
+// createWeb3Modal({ wagmiConfig, projectId, chains });
 
 WebApp.ready();
 
 function App() {
   const [count, setCount] = useState(0);
-
-  const { open } = useWeb3Modal();
+  // const { open } = useWeb3Modal();
 
   const action1 = async () => {
     WebApp.HapticFeedback.impactOccurred("light");
@@ -56,115 +44,104 @@ function App() {
     WebApp.CloudStorage.getItem("test", (_, result) => console.log(result));
   };
 
-  async function resolveBnb(name: string) {
-    const rpc = "https://data-seed-prebsc-1-s1.binance.org:8545/";
-    const provider = new providers.JsonRpcProvider(rpc);
-    sid = new SID({ provider, sidAddress: SIDfunctions.getSidAddress("97") });
-    const address = await sid.name(name).getAddress(); // 0x123
-    console.log("name: %s, address: %s", name, address);
-  }
+  // async function resolveBnb(name: string) {
+  //   let sid;
+  //   const rpc = "https://data-seed-prebsc-1-s1.binance.org:8545/";
+  //   const provider = new providers.JsonRpcProvider(rpc);
+  //   sid = new SID({ provider, sidAddress: SIDfunctions.getSidAddress("97") });
+  //   const address = await sid.name(name).getAddress(); // 0x123
+  //   console.log("name: %s, address: %s", name, address);
+  // }
 
   async function resolveBnb2(name: string) {
-    const response = await fetch(`https://api.prd.space.id/v1/getAddress?tld=bnb&domain=${name}`);
-    const data = await response.json()
-    WebApp.showAlert("resolved")
-    console.log("🚀 ~ file: App.tsx:70 ~ resolveBnb ~ data:", data)
+    const response = await fetch(
+      `https://api.prd.space.id/v1/getAddress?tld=bnb&domain=${name}`
+    );
+    const data = await response.json();
+    WebApp.showAlert("resolved");
+    console.log("🚀 ~ file: App.tsx:70 ~ resolveBnb ~ data:", data);
   }
 
   return (
     <>
-      <WagmiConfig config={wagmiConfig}>
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <button onClick={() => resolveBnb("felix.bnb")}>resolveBnb</button>
-          <button onClick={() => resolveBnb2("felix.bnb")}>resolveBnb2</button>
+      <div>
+        <button onClick={() => resolveBnb2("felix.bnb")}>resolveBnb2</button>
 
-          <button onClick={() => WebApp.expand()}>Expand</button>
-          <button
-            onClick={() => {
-              WebApp.initData;
-              console.log(
-                "🚀 ~ file: App.tsx:63 ~ App ~ ebApp.initData:",
-                WebApp.initData
-              );
-            }}
+        <button onClick={() => WebApp.expand()}>Expand</button>
+        <button
+          onClick={() => {
+            WebApp.initData;
+            console.log(
+              "🚀 ~ file: App.tsx:63 ~ App ~ ebApp.initData:",
+              WebApp.initData
+            );
+          }}
+        >
+          initData
+        </button>
+        <button onClick={() => WebApp.showScanQrPopup({ text: "hello" })}>
+          showScanQrPopup
+        </button>
+        <button onClick={() => WebApp.switchInlineQuery("switch to inline")}>
+          switchInlineQuery
+        </button>
+
+        <MainButton text="Close" onClick={() => WebApp.close()} />
+        {/* <BackButton onClick={() => alert("back")} /> */}
+        <Page mode="secondary">
+          <Section
+            description="Share your contacts with the community. It will help other members to reach you faster."
+            header="Public contacts"
           >
-            initData
-          </button>
-          <button onClick={() => WebApp.showScanQrPopup({ text: "hello" })}>
-            showScanQrPopup
-          </button>
-          <button onClick={() => WebApp.switchInlineQuery("switch to inline")}>
-            switchInlineQuery
-          </button>
+            <Cell after={<Switch />} description="@Stambultsian">
+              Twitter
+            </Cell>
+            <Cell after={<Switch />} description="@artursupertramp">
+              Facebook
+            </Cell>
+            <Cell after={<Switch checked />} description="@ArthurStam">
+              Telegram
+            </Cell>
+          </Section>
+        </Page>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <button type="button" onClick={() => WebApp.showAlert("Hey there!")}>
+          click
+        </button>
+        <button type="button" onClick={action1}>
+          click1
+        </button>
+        <button type="button" onClick={action2}>
+          click2
+        </button>
+        <button type="button" onClick={action3}>
+          click3
+        </button>
 
-          <MainButton text="Close" onClick={() => WebApp.close()} />
-          {/* <BackButton onClick={() => alert("back")} /> */}
-          <Page mode="secondary">
-            <Section
-              description="Share your contacts with the community. It will help other members to reach you faster."
-              header="Public contacts"
-            >
-              <Cell after={<Switch />} description="@Stambultsian">
-                Twitter
-              </Cell>
-              <Cell after={<Switch />} description="@artursupertramp">
-                Facebook
-              </Cell>
-              <Cell after={<Switch checked />} description="@ArthurStam">
-                Telegram
-              </Cell>
-            </Section>
-          </Page>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <button type="button" onClick={() => WebApp.showAlert("Hey there!")}>
-            click
-          </button>
-          <button type="button" onClick={action1}>
-            click1
-          </button>
-          <button type="button" onClick={action2}>
-            click2
-          </button>
-          <button type="button" onClick={action3}>
-            click3
-          </button>
+        <InitialsAvatar
+          entityId={12345}
+          entityName="Artur Stambultsian"
+          theme="apple"
+          className="MyAvatar"
+          style={{ marginRight: 10 }}
+        />
 
-          <button onClick={() => open()}>Open Connect Modal</button>
-          <button onClick={() => open({ view: "Networks" })}>
-            Open Network Modal
-          </button>
-
-          <InitialsAvatar
-            entityId={12345}
-            entityName="Artur Stambultsian"
-            theme="apple"
-            className="MyAvatar"
-            style={{ marginRight: 10 }}
-          />
-
-          <ImageAvatar
-            size={100}
-            src="https://cdn4.telegram-cdn.org/file/GSG98YPeYhPH42R0BlvQT7di6ha48LJI4lZwbQ7q7f_TYAC5FwRrEBTlyaVij-ylfflg4aU0R8AtL8UZmG9R6sX0S12R_zLTuEUlaV99qXZm_OcaFqdknYCbxb7-XuVCy7c7mPaWagdFuF_T2-fopi1dzsrvW6E6ff8qFhQqIrrvtBdq7jO5829aCEpSIHi9QqFuw6l1WuOWfoq1LjjV7kq2wSfo_uOlN4PP-hFPAL1wklwnTw4NPE-Mfu9gFd472JUN9e299UP2f2jeZnvst2qX0Go6fb81gewClahABP-veeqGrVBxVphfixV3KJulKdt0CCU7KhFM7e-Gd4-qug.jpg"
-          />
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
+        <ImageAvatar
+          size={100}
+          src="https://cdn4.telegram-cdn.org/file/GSG98YPeYhPH42R0BlvQT7di6ha48LJI4lZwbQ7q7f_TYAC5FwRrEBTlyaVij-ylfflg4aU0R8AtL8UZmG9R6sX0S12R_zLTuEUlaV99qXZm_OcaFqdknYCbxb7-XuVCy7c7mPaWagdFuF_T2-fopi1dzsrvW6E6ff8qFhQqIrrvtBdq7jO5829aCEpSIHi9QqFuw6l1WuOWfoq1LjjV7kq2wSfo_uOlN4PP-hFPAL1wklwnTw4NPE-Mfu9gFd472JUN9e299UP2f2jeZnvst2qX0Go6fb81gewClahABP-veeqGrVBxVphfixV3KJulKdt0CCU7KhFM7e-Gd4-qug.jpg"
+        />
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-      </WagmiConfig>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   );
 }
