424342b043ee614028723fb110db438ab3726627
aeither <36173828+aeither@users.noreply.github.com>
2023-09-24 13:06:10 +0300

    add backend

---
+++ backend/src/index.ts
@@ -0,0 +1,22 @@
+import { Elysia } from "elysia";
+
+interface TransactionData {
+  code: number;
+  address: string;
+}
+
+
+const app = new Elysia()
+  .get("/", () => "Hello!!!")
+  .get("/:name", async ({ params: { name } }) => {
+    const response = await fetch(
+      `https://api.prd.space.id/v1/getAddress?tld=bnb&domain=${name}`
+    );
+    const data: TransactionData = await response.json();
+    return data.address // return 0x0000000000000000000000000000000000000000 if does not exist
+  })
+  .listen(8080);
+
+console.log(
+  `🦊 Elysia is running at on port ${app.server?.port}... \n Open: http://localhost:8080`
+);
