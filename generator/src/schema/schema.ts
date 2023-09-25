631b11830f68f0b62056a0a4a883f05f5a7d702b
aeither <36173828+aeither@users.noreply.github.com>
2023-09-26 00:31:47 +0300

    add primary key

--- generator/src/schema/schema.ts
+++ generator/src/schema/schema.ts
@@ -1,7 +1,7 @@
 import { serial, text, timestamp, pgTable } from "drizzle-orm/pg-core";
- 
+
 export const user = pgTable("user", {
-  id: serial("id"),
+  id: serial("id").primaryKey(),
   name: text("name"),
   role: text("role").$type<"bnb" | "arb">(),
   createdAt: timestamp("created_at"),
